beb093cdb9f61d03df9595216d03fc0b3bf19be7
desnunan <desnunan@hotmail.com>
2020-01-14 16:37:04 +0000

    Update commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Hello World from noVNC container!"
+echo "Here is the GitHub workspace containing our source"
 pwd
+cd ~
+echo "And here is the home directory within the noVNC container"
+pwd
+
